f6de4fc81eada0a3f962be267aeca854e5a93a52
Gaoyinghao <18600408534@163.com>
2018-07-18 12:03:56 +0800

    修改json

--- websiteHomePage/appWebsiteHomePage.js
+++ websiteHomePage/appWebsiteHomePage.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser=require('body-parser');
 
 var app = express();
 app.set('views', './');
@@ -8,9 +7,6 @@ app.use(express.static(__dirname + '/'));
 
 app.use('/', express.static(__dirname + '/'));
 app.engine( '.html', require( 'ejs' ).__express );
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
   res.render('index')
